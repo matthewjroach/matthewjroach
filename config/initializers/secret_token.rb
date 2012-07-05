628bad8a259a1634230bc78229d4d0f733cabb40
Matthew Roach <mjroach@gmail.com>
2012-07-05 02:42:49 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matthewjroach::Application.config.secret_token = '5c3e9061af090ea2ade73000179990b207fe500e1e6305b090850d7dacd7d1e56857e92b3d97baf49197557cbf8a7e37746b18bec1244552cb5c359232a38be0'
